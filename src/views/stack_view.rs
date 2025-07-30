4a74afca9afe2f35850facf750dc955a75223f39
ryanrixxh <ryanmbr@protonmail.com>
2025-07-30 16:20:15 +0930

    listing seperate stack categories sorted by resource type

--- src/views/stack_view.rs
+++ src/views/stack_view.rs
@@ -1,16 +1,24 @@
 use crate::aws::get_stack_resources;
 use aws_sdk_cloudformation::types::{StackResource, StackSummary};
+
 use ratatui::{
     buffer::Buffer,
-    layout::Rect,
+    layout::{Constraint, Direction, Layout, Rect},
     widgets::{Block, Clear, List, Widget},
 };
 use std::sync::Arc;
 use tokio::runtime::Builder;
 
 pub struct Stack {
-    stack_summary: Arc<StackSummary>,
-    resources: Vec<StackResource>,
+    _stack_summary: Arc<StackSummary>,
+    resources: Resources,
+}
+
+#[derive(Debug, Clone)]
+struct Resources {
+    lambdas: Vec<StackResource>,
+    state_machines: Vec<StackResource>,
+    apis: Vec<StackResource>,
 }
 
 impl Stack {
@@ -25,28 +33,83 @@ impl Stack {
         // TODO: This is still blocking the main UI thread from rendering. Need to figure out a way
         // to render an empty list, and then re-render one the runtime has grabbed what it needs
         // from AWS
+        // It would actually be WAY better if cached this!
         let resources = runtime.block_on(handle).unwrap().unwrap();
+
         Self {
-            stack_summary,
-            resources,
+            _stack_summary: stack_summary,
+            resources: Self::sort(resources),
         }
     }
-}
 
-impl Widget for &Stack {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        Clear.render(area, buf);
+    fn sort(resources: Vec<StackResource>) -> Resources {
+        let mut sorted = Resources {
+            lambdas: vec![],
+            state_machines: vec![],
+            apis: vec![],
+        };
+
+        resources.iter().for_each(|resource| -> () {
+            match resource.resource_type() {
+                Some("AWS::Lambda::Function") => sorted.lambdas.push(resource.clone()),
+                Some("AWS::StepFunctions::StateMachine") => {
+                    sorted.state_machines.push(resource.clone())
+                }
+                Some("AWS::ApiGatewayRestApi") => sorted.apis.push(resource.clone()),
+                Some(_x) => (),
+                None => (),
+            }
+        });
+
+        return sorted;
+    }
+
+    fn render_list(
+        &self,
+        area: Rect,
+        buf: &mut Buffer,
+        resources: &Vec<StackResource>,
+        title: &str,
+    ) -> () {
         Widget::render(
             List::new::<Vec<&str>>(
-                self.resources
+                resources
                     .clone()
                     .iter()
                     .map(|resource| resource.logical_resource_id().unwrap())
                     .collect(),
             )
-            .block(Block::bordered().title(self.stack_summary.stack_name().unwrap())),
+            .block(Block::bordered().title(title)),
             area,
             buf,
         );
+    }
+}
+
+impl Widget for &Stack {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let layout = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(vec![
+                Constraint::Percentage(33),
+                Constraint::Percentage(33),
+                Constraint::Percentage(33),
+            ])
+            .split(area);
+        Clear.render(area, buf);
+
+        self.render_list(layout[0], buf, &self.resources.lambdas, "Lambda Functions");
+        self.render_list(
+            layout[1],
+            buf,
+            &self.resources.state_machines,
+            "State Machines",
+        );
+        self.render_list(
+            layout[2],
+            buf,
+            &self.resources.apis,
+            "API Gateway Rest APIs",
+        )
     }
 }
